2a5f33b7e0236b81344524dd6729025c95e5c77d
Dell <Dell@DESKTOP-G77TL2P>
2024-07-29 23:01:16 +0530

    New testcase

---
+++ Gitnew/src/gitnewpackage/Gitclass.java
@@ -0,0 +1,22 @@
+package gitnewpackage;
+
+public class Gitclass {
+
+	public static void main(String[] args) {
+		
+		String b="10";
+		
+	  int i =Integer.parseInt(b);
+	 System.out.println(i);
+	 
+	 int a= 5000;
+	 String t= String.valueOf(a);
+	 System.out.println(t.length());
+	
+		
+		
+		
+
+	}
+
+}
